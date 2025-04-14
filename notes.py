55bdf4d53e2a3b8e934903072f2c23d923937bec
John C. Keenan <jck8094@uncw.edu>
2025-04-13 21:08:02 -0400

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,82 @@
+# John Keenan
+# 4/13/25
+# CSC450-002
+# Professor Devon Simmonds
+# Notes class
+class Notes:
+    def __init__(self, title, body="LOAD"):
+        """
+        Constructor.
+        :param title: str   will be the name of the textfile stored
+        :param body: str    the notes content; optional parameter. If not passed, will try to open <title>.txt, and its
+                            contents will be the body
+        """
+        self.title = title
+        if body == "LOAD":              # the default value of the body parameter
+            file = open(title+".txt")   # open a textfile with a name equal to the title passed
+            file.readline()             # first line says "can(not) be read as a deck"
+
+            # populate the body with the contents of the text file
+            self.body = ""
+            for line in file:
+                self.body += line
+
+        else:
+            self.body = body
+
+    def get_title(self):
+        return self.title
+
+    def set_title(self, title):
+        self.title = title
+
+    def get_body(self):
+        return self.body
+
+    def set_body(self, body):
+        self.body = body
+
+    def __is_valid_deck(self):
+        """
+        Private function, used when storing.
+        :return:
+        """
+        lines = self.body.split("\n")
+        for line in lines:
+            front_back = line.split(":")
+
+            # a text file can only be read as a deck if each line has a colon
+            if len(front_back) != 2:
+                return False
+        return True
+
+    def store(self):
+        """
+        save the note to a text file
+        :return: NoneType
+        """
+        file = open(self.title+".txt", 'w')
+
+        # a header indicating whether it can be read as a deck or not
+        file.write("can be read as a deck\n" if self.__is_valid_deck() else "cannot be read as a deck\n")
+
+        file.write(self.body)
+
+    def __str__(self):
+        return self.body
+
+
+def main():
+    notes = Notes("Math", """1+1: 2
+2+2: 4
+3+3""")
+
+    print(notes)
+    notes.store()
+
+    math_notes = Notes("Math")
+    print(notes)
+
+
+if __name__ == '__main__':
+    main()
